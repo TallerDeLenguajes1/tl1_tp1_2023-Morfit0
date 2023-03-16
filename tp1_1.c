a975be0f8350572449a7a894057c3218cd7b2cb3
--replace-all <inakipoch106@gmail.com>
2023-03-16 19:30:13 -0300

    Added a pointer and several values of it

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola mundo");
+    int num, *pNum;
+    pNum = &num;
+    printf("Hola mundo\n");
+    printf("%p\n",pNum);
+    printf("%p\n",&pNum);
+    printf("%d\n",sizeof(pNum));
+    printf("%d\n",*pNum);
+    
     return 0;
 }
